c473b57b334f7e52c2eb5e6aa3dab5277d395f9e
Jimgao <19855812775@163.com>
2020-10-27 19:57:50 +0800

    Update index.js
    reference: https://github.com/spencerwooo/spencerwooo

--- index.js
+++ index.js
@@ -17,23 +17,12 @@ function generateProgressBar() {
 const readme = `
 <h2>Hi, I'm Jimgao 👋👨‍💻</h2>
 
----
-
 Annual balance   ${progressBarOfThisYear}  ${(progressOfThisYear * 100).toFixed(2)} %
 
----
-
-![GitHub visiters](https://komarev.com/ghpvc/?username=tianheg) &nbsp;&nbsp; ![GitHub followers](https://img.shields.io/github/followers/tianheg?label=Follow&style=social)
-
----
-
-### A little more about me...
-
-    const Jimgao = {
-        pronouns: "Jimgao" | "he",
-        code: [Python, Javascript, HTML, CSS]
-    }
-
+- ⚡ JavaScript / Python . Vue.
+- 🔑 GPG key hosted here at [GitHub](https://github.com/tianheg.gpg) and also [Keybase](https://keybase.io/yidajiabei/pgp_keys.asc), see [Keybase proof](https://gist.github.com/tianheg/1ce40c3e06eddab6bc72b87cc26ec067).
+- 📫 Email me at: [yidajiabei@gmail.com](mailto:yidajiabei@gmail.com)
+- 💰 Support my work on [爱发电](https://afdian.net/@yidajiabei)!
 
 ![Jimgao's github stats](https://github-readme-stats-yidajiabei.vercel.app/api?username=tianheg&show_icons=true)
 
