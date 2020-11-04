e150f92638a37ee37c7c6b8c908644e194de5a83
Jimgao <19855812775@163.com>
2020-11-04 22:13:52 +0800

    Update index.js
    rm repos and sites

--- index.js
+++ index.js
@@ -28,37 +28,6 @@ Annual balance   ${progressBarOfThisYear}  ${(progressOfThisYear * 100).toFixed(
 
 <em><b>I love connecting with different people</b> so if you want to say <b>hi, I'll be happy to meet you more!</b> :)</em>
 
-Below repos are often used, be free 🤪
-
-- [portfolio-vue](https://github.com/tianheg/portfolio-vue)
-- [Changelog](https://github.com/tianheg/Changelog)
-- [self-watching](https://github.com/tianheg/self-watching)
-- [wiki-docs](https://github.com/tianheg/wiki-docs)
-- [Coding](https://github.com/tianheg/coding)
-- [yidajiabei.xyz](https://github.com/tianheg/yidajiabei.xyz)
-- [English](https://github.com/tianheg/English)
-- [site](https://github.com/tianheg/site)
-- [dotfiles](https://github.com/tianheg/dotfiles)
-- [knowledgebase](https://github.com/tianheg/knowledgebase)
-- [docker-docs](https://github.com/tianheg/docker-docs)
-- [traffic-badge](https://github.com/tianheg/traffic-badge)
-
-Below sites are often used, be free 🤪
-
-- [portfolio](https://github.com/tianheg/portfolio-vue)
-- [Blog](https://blog.yidajiabei.xyz)
-- [Changelog](https://tianheg.github.io/Changelog)
-- [Coding](https://tianheg.github.io/coding)
-- [English words](https://tianheg.github.io/English)
-- [Knowledgebase](https://gaotianhe.github.io/knowledgebase)
-- [Sites](https://tianheg.github.io/site)
-- [Gitmemory](https://www.gitmemory.com/tianheg)
-- [Travis CI](https://travis-ci.com/dashboard)
-- [Wakatime](https://wakatime.com/dashboard)
-- [Cheatsheets](https://tianheg.github.io/cheatsheets)
-- [Linux Command](https://tianheg.github.io/linux-command)
-- [Docker Docs](https://tianheg.github.io/docker-docs)
-
 ---
 
 ⏰ Updated on ${new Date().toUTCString()}`
