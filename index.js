c903545939a212f8dcd88aaf8184f65579d9bc9d
Jimgao <19855812775@163.com>
2020-12-09 00:22:42 +0800

    add DouBan

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ teach_yourself(coding)
 - 🔑 GPG key hosted here at [GitHub](https://github.com/tianheg.gpg) and also [Keybase](https://keybase.io/yidajiabei/pgp_keys.asc), see [Keybase proof](https://gist.github.com/tianheg/1ce40c3e06eddab6bc72b87cc26ec067).
 - 📫 Email me at: [yidajiabei@gmail.com](mailto:yidajiabei@gmail.com)
 - 💰 Support my work on [爱发电](https://afdian.net/@yidajiabei)!
-- 🌐 [Linkedin](https://www.linkedin.com/in/tianheg/)
+- 🌐 [Linkedin](https://www.linkedin.com/in/tianheg/) &bull; [DouBan](https://www.douban.com/people/yidajiabei/)
 
 <em><b>I love connecting with different people</b> so if you want to say <b>hi, I'll be happy to meet you more!</b> :)</em>
 
