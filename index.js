6157371be865c52d5bec43c053853bf3c64d312d
Jimgao <19855812775@163.com>
2020-12-08 22:29:36 +0800

    add Linkedin profile

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ teach_yourself(coding)
 - 🔑 GPG key hosted here at [GitHub](https://github.com/tianheg.gpg) and also [Keybase](https://keybase.io/yidajiabei/pgp_keys.asc), see [Keybase proof](https://gist.github.com/tianheg/1ce40c3e06eddab6bc72b87cc26ec067).
 - 📫 Email me at: [yidajiabei@gmail.com](mailto:yidajiabei@gmail.com)
 - 💰 Support my work on [爱发电](https://afdian.net/@yidajiabei)!
+- 🌐 [![Linkedin](https://img.shields.io/badge/linkedin%20-%23007ACC.svg?&style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/tianheg/)
 
 <em><b>I love connecting with different people</b> so if you want to say <b>hi, I'll be happy to meet you more!</b> :)</em>
 
