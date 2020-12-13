aa0f0b55f61ad3a4fdcdb384303790588ec18c38
Jimgao <19855812775@163.com>
2020-12-13 19:08:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ teach_yourself(coding)
 - 💰 Support my work on [爱发电](https://afdian.net/@yidajiabei)!
 - 🌐 [Linkedin](https://www.linkedin.com/in/tianheg/) &bull; [DouBan](https://www.douban.com/people/yidajiabei/)
 
-<h3>Current work</h3>:
+<h3>Current work</h3>
 
 [![README Card](https://tianheg-readme-stats.vercel.app/api/pin/?username=tianheg&repo=build-my-first-app)](https://github.com/tianheg/build-my-first-app)
 
