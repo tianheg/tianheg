51f9c64a3478e55a0fc25d812229fdf83a7b5eae
tianheg <19855812775@163.com>
2020-12-26 23:00:35 +0800

    Update index.js
    remove current work

--- index.js
+++ index.js
@@ -35,10 +35,6 @@ teach_yourself(coding)
 - 💰 Support my work on [爱发电](https://afdian.net/@yidajiabei)!
 - 🌐 [Linkedin](https://www.linkedin.com/in/tianheg/) &bull; [DouBan](https://www.douban.com/people/yidajiabei/)
 
-<h3>Current work</h3>
-
-[![README Card](https://tianheg-readme-stats.vercel.app/api/pin/?username=tianheg&repo=RSS-reader)](https://github.com/tianheg/RSS-reader)
-
 <em><b>I love connecting with different people</b> so if you want to say <b>hi, I'll be happy to meet you more!</b> :)</em>
 
 <img src="https://tianheg-readme-stats.vercel.app/api?username=tianheg&show_icons=true"/>
