f2718e117f78cd391a4228bb5fe6f500c9515d7e
tianheg <19855812775@163.com>
2020-12-29 07:45:32 +0800

    Update index.js
    use Afdian replace 爱发电

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ teach_myself(coding)
 - ⚡ JavaScript / Python . Vue.
 - 🔑 GPG key hosted here at [GitHub](https://github.com/tianheg.gpg) and also [Keybase](https://keybase.io/yidajiabei/pgp_keys.asc), see [Keybase proof](https://gist.github.com/tianheg/1ce40c3e06eddab6bc72b87cc26ec067).
 - 📫 Email me at: yidajiabei#gmail.com
-- 💰 Support my work on [爱发电](https://afdian.net/@yidajiabei)!
+- 💰 Support my work on [Afdian](https://afdian.net/@yidajiabei)!
 - 🌐 [Linkedin](https://www.linkedin.com/in/tianheg/) &bull; [DouBan](https://www.douban.com/people/yidajiabei/)
 
 <em><b>I love connecting with different people</b> so if you want to say <b>hi, I'll be happy to meet you more!</b> :)</em>
