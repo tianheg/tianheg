dfcd9726deb0a91de354b2945b0e4a69e1d496ee
Jim Gao <yidajiabei@163.com>
2021-09-25 23:14:51 +0000

    add(blog_en)

--- main.py
+++ main.py
@@ -27,7 +27,8 @@ def get_link_info(feed_url, num):
 
 def main():
     blog =  get_link_info("https://blog.yidajiabei.xyz/index.xml", 3)
-    insert_info = blog
+    blog_en =  get_link_info("https://blog.yidajiabei.xyz/en/index.xml", 3)
+    insert_info = blog + blog_en
 
     # 替换 ---start--- 到 ---end--- 之间的内容
     # pytz.timezone('Asia/Shanghai')).strftime('%Y年%m月%d日%H时M分')
