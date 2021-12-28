658c0fd721bc0bc60b829c93fe2f856e53b4b442
Jim Gao <yidajiabei@163.com>
2021-12-28 17:58:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,19 +19,14 @@ const readme = `
 
 Annual balance   ${progressBarOfThisYear}  ${(progressOfThisYear * 100).toFixed(2)} %
 
-- ⚡ HTML, CSS, JavaScript.
+- ⚡ HTML5, CSS3, JavaScript(ES6).
 - 🔑 GPG key hosted here at [GitHub](https://github.com/tianheg.gpg).
+- :email: [me@yidajiabei.xyz](mailto:me@yidajiabei.xyz).
 - 💰 Support my work on [Patreon](https://www.patreon.com/tianheg) or [Afdian](https://afdian.net/@tianheg)!
 
 <em><b>I love connecting with different people</b> so if you want to say <b>hi, I'll be happy to meet you more!</b> :)</em>
 
 ---
 
-⏰ Updated on ${new Date().toString()}
-
-[How this works](https://github.com/tianheg/tianheg/issues/1)
-
-![GitHub Stats Card](https://tianheg-readme-stats.vercel.app/api?username=tianheg&show_icons=true)
-
-![Wakatime Card](https://tianheg-readme-stats.vercel.app/api/wakatime?username=tianheg&layout=compact)`;
+⏰ Updated on ${new Date().toString()}`;
 console.log(readme)
