9dde19eef10a379262ed3c854ae7c17012476d73
tianheg <i@tianheg.co>
2024-05-04 10:53:09 +0800

    add blog updates and contributions

--- README.md
+++ README.md
@@ -6,3 +6,33 @@
 
 <em><b>I love connecting with different people</b> so if you want to say <b>hi, I'll be happy to meet you more!</b> :)</em>
 
+---
+
+![GitHub Stats Card](https://github-readme-stats.tianheg.org/api?username=tianheg&show_icons=true)
+
+<table><tr><td valign="top" width="55%">
+
+### On my blog
+- [Hugo Add . Keyboard Shortcut](https://tianheg.co/posts/hugo-add-point-keyboard-shortcut/)
+- [光 - 王三溥](https://tianheg.co/posts/light-wang-sanpu/)
+- [幸福的芝麻 - 张浅潜](https://tianheg.co/posts/happy-sesame-zhang-qianqian/)
+- [最好的时光 - 安溥 anpu](https://tianheg.co/posts/best-of-times-anpu/)
+- [What it's like to run](https://tianheg.co/posts/what-its-like-to-run-en/)
+- [跑步的感受](https://tianheg.co/posts/what-its-like-to-run/)
+- [Youth by Samuel Ullman](https://tianheg.co/posts/youth-by-samuel-ullman/)
+- [狗叫](https://tianheg.co/posts/dog-barking/)
+- [八九点钟的太阳 - 熊熊作业](https://tianheg.co/posts/eight-or-nine-oclock-sun-xiongxiong-zuoye/)
+- [双语写作一百天](https://tianheg.co/posts/one-hundred-days-of-bilingual-writing/)
+
+</td><td valign="top" width="55%">
+
+### Recent contributions
+
+- [sass/dart-sass](https://github.com/sass/dart-sass)
+- [garritfra/garrit.xyz](https://github.com/garritfra/garrit.xyz)
+- [sissbruecker/linkding](https://github.com/sissbruecker/linkding)
+- [timqian/chinese-independent-blogs](https://github.com/timqian/chinese-independent-blogs)
+- [yihong0618/running_page](https://github.com/yihong0618/running_page)
+</td></tr></table>
+
+[![Update profile](https://github.com/tianheg/tianheg/actions/workflows/main.yml/badge.svg?branch=main)](https://github.com/tianheg/tianheg/actions/workflows/main.yml)
--- index.js
+++ index.js
@@ -1,11 +1,77 @@
-// common
-const readme = `<h2>Hi, I'm tianheg 👋👨‍💻</h2>
+import { graphql } from "@octokit/graphql";
+import Parser from 'rss-parser';
 
-- 📖 I enjoy [READing](https://tianheg.co/tags/reading/).
-- 📝 I record [notes, blog](https://tianheg.co), Chinese mostly.
-- 💰 [Support my work](https://github.com/tianheg/sponsor).
+// env
+import dotenv from "dotenv";
+dotenv.config();
+
+
+const intro = `<h2>Hi, I'm tianheg 👋👨‍💻</h2>
+
+- 📖 I enjoy [READing](https://read.tianheg.org/).
+- 📝 I record [notes, blog](https://tianheg.co).
+- 💰 [Support my work](https://tianheg.co/support).
 
 <em><b>I love connecting with different people</b> so if you want to say <b>hi, I'll be happy to meet you more!</b> :)</em>
 `;
 
-console.log(readme);
+// rss
+const parser = new Parser();
+const feed = await parser.parseURL('https://tianheg.co/index.xml');
+
+// getContributedRepos
+const graphqlWithAuth = graphql.defaults({
+  headers: {
+    authorization: `token ${process.env.ACCESS_TOKEN}`,
+  },
+});
+
+const query = `{
+  viewer {
+    repositoriesContributedTo(
+      first: 100
+      contributionTypes: [COMMIT]
+    ) {
+      totalCount
+      nodes {
+        nameWithOwner
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+    }
+  }
+}
+`;
+
+const { viewer } = await graphqlWithAuth(query);
+const contributedRepos = viewer.repositoriesContributedTo.nodes
+  .map(
+    (repo) =>
+      `- [${repo.nameWithOwner}](https://github.com/${repo.nameWithOwner})`
+  )
+  .join("\n");
+
+
+const readmeContent =
+  `${intro}
+---
+
+![GitHub Stats Card](https://github-readme-stats.tianheg.org/api?username=tianheg&show_icons=true)
+
+<table><tr><td valign="top" width="55%">
+
+### On my blog
+${feed.items.map((item) => `- [${item.title}](${item.link})`).join("\n")}
+
+</td><td valign="top" width="55%">
+
+### Recent contributions
+
+${contributedRepos}
+</td></tr></table>
+
+[![Update profile](https://github.com/tianheg/tianheg/actions/workflows/main.yml/badge.svg?branch=main)](https://github.com/tianheg/tianheg/actions/workflows/main.yml)`;
+
+console.log(readmeContent);
