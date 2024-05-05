5cde25f03a25e1b234a95be6b196c3b90f17abcd
tianheg <i@tianheg.co>
2024-05-05 21:01:15 +0800

    update index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const intro = `<h2>Hi, I'm tianheg 👋👨‍💻</h2>
 - 📖 I enjoy [READing](https://read.tianheg.org/).
 - 📝 I record [notes, blog](https://tianheg.co).
 - 💰 [Support my work](https://tianheg.co/support).
+- [freeCodeCamp profile](https://www.freecodecamp.org/tianheg).
 
 <em><b>I love connecting with different people</b> so if you want to say <b>hi, I'll be happy to meet you more!</b> :)</em>
 `;
